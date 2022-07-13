e0829662fa0295d8a00b91b11ec891718d05bf87
Aislu Baisova <ice_lu_93@mail.ru>
2022-07-13 21:37:00 +0300

    Change a bit

--- 1.js
+++ 1.js
@@ -4,9 +4,10 @@ function chislo(num){
     } else if (num % 3 == 0){
     console.log('Fizz');
     } else if (num % 5 == 0){
-    console.log('Buzz')
+    console.log('Buzz');
+    } else console.log('Юзер, ты дурак, твое число не делится ни на 3, ни на 5 воообщееее' )
     }
-}
 chislo (3);
 chislo (5);
 chislo (15);
+chislo (2);
