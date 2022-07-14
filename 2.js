1ca56fc70b240c3b11bb259096f4d16342214688
Aislu Baisova <ice_lu_93@mail.ru>
2022-07-14 19:58:01 +0300

    Add a new training

---
+++ 2.js
@@ -0,0 +1,19 @@
+function calculator (num1, num2, operation){
+    if (operation == 'plus'){
+        const sum1 = num1+num2
+        console.log (sum1)
+    } else if (operation == 'minus'){
+        const sum2 = num1-num2
+        console.log (sum2)
+    } else if (operation == 'umnozhit'){
+        const sum3 = num1*num2
+        console.log (sum3)
+    } else if (operation == 'razdelit'){
+        const sum4 = num1/num2
+        console.log (sum4)
+        }
+
+
+    }
+
+calculator (5,2,'razdelit')
